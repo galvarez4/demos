b34bef2ca17f1b61003595ea0edd690287add1ea
galvarez4 <galvarez4@hawk.iit.edu>
2025-12-02 11:46:43 -0600

    idk

---
+++ 03_flutter_nav_route/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\gisel\OneDrive\Documents\GitHub\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\gisel\OneDrive\Documents\GitHub\demos\03_flutter_nav_route"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
